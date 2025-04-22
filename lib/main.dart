ac74b10013ab9dd26d2811f590a708b1f00731f5
Victor-Valedev <victorvaledev@gmail.com>
2025-04-22 13:59:23 -0300

    feat: adiciona um menu lateral para a tela de configurações e refeições

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mealsapp/screens/categories_meals_screen.dart';
 import 'package:mealsapp/screens/categories_screen.dart';
 import 'package:mealsapp/screens/meal_detail_screen.dart';
+import 'package:mealsapp/screens/settings_screen.dart';
 import 'package:mealsapp/screens/tabs_screen.dart';
 import 'package:mealsapp/utils/app_routes.dart';
 
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen()
       },
     );
   }
